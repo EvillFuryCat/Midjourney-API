796c131aaee6f8d1ce5e95c8961704582a79668a
Konstantin <k1st.volkov@gmail.com>
2023-07-17 13:09:21 +0300

    Теперь по POST запросу send-message отправляется корректный json для бота midjourney

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import discord
-
-from discord.ext import commands
-
-
-from fastapi import FastAPI, HTTPException
+import json
+from fastapi import FastAPI, Request, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import discord
 import requests
+import httpx
+from discord.ext import commands
 
 
 # Создай модель для сообщения
@@ -15,31 +14,50 @@ class Item(BaseModel):
 
 
 CHANEL_ID = 285724843557781505
-CLIENT_TOKEN = "Mjg1MDgwMDM4NTUzMTU3NjMy.Ghtosg.EivjT7-k4jQswD4fzR3P1gQotLv12g8uhisaLE"
+CLIENT_TOKEN = "Mjg1MDgwMDM4NTUzMTU3NjMy.GiOReY.VR0aJJa-DIx87babkNrNbVA7xi4t6l52yqC5DQ"
 
 app = FastAPI()
 
 
+
+
 @app.post("/send-message")
-async def send_message_route(message: str):
-    headers = {
-        'Authorization': f'{CLIENT_TOKEN}',
-        'Content-Type': 'application/json'
-    }
-
+async def send_message_route(prompt: str):
+    headers = {"Authorization": f"{CLIENT_TOKEN}", "Content-Type": "application/json", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
     data = {
-        'content': f"/imagine prompt:{message}"
-    }
+   "type":2,
+   "application_id":"936929561302675456",
+   "guild_id":"285724843557781505",
+   "channel_id":"285724843557781505",
+   "session_id":"effacc7b-e32c-4002-8f61-d4b471fe22c7",
+   "data":{
+      "version":"1118961510123847772",
+      "id":"938956540159881230",
+      "name":"imagine",
+      "type": 1,
+      "options":[
+         {
+            "type":3,
+            "name":"prompt",
+            "description":"What is the question?",
+            "required":True,
+            "value": f"{prompt}"
+         }
+      ]
+   }
+}
 
-    url = f'https://discord.com/api/v9/channels/{CHANEL_ID}/messages'
-    response = requests.post(url, headers=headers, json=data)
-
-    if response.status_code == 200:
+    url = "https://discord.com/api/v9/interactions"
+    response = requests.post(url, headers=headers, json=data, )
+    if response.status_code == 204:
         return {"message": "Message sent successfully"}
     else:
         return {"message": "Failed to send message"}
-    
-    
-@app.post('/image/')
-async def send_image_url(item: Item):
-    return JSONResponse(status_code=200, content=item.message)
+
+
+@app.post("/image/")
+async def process_image(image: UploadFile):
+    file_path = f"images/{image.filename}"
+    with open(file_path, "wb") as f:
+        f.write(await image.read())
+    return {"message": "Image uploaded successfully"}
--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
-from discord import Client, Intents, Message, Attachment, User
-
+from discord import Client, Intents, Message
 import aiohttp
+import requests
 
 BOT_TOKEN = "MTEyODk3MTg4NjUwNDcxODM0Nw.GjdeFa.HiRVLZ-Ay_u6zI9nlQvUKdslfSl7ennSdby4Uw"
 
 
 class MyClient(Client):
+    def __init__(self, api_url):
+        super().__init__(intents=Intents.all())
+        self.api_url = api_url
+    
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print("Logged on as", self.user)
+
 
     async def on_message(self, message: Message):
         # Не реагируем на сообщения от нас самих
@@ -19,13 +24,17 @@ class MyClient(Client):
             for attachment in message.attachments:
                 if attachment.url.lower().endswith(("png", "jpg", "jpeg")):
                     # Верни URL изображения
-                    async with aiohttp.ClientSession() as session:
-                        async with session.get(attachment.url) as resp:
-                            if resp.status == 200:
-                                # Сохранить изображение (можно выбрать нужный путь)
-                                with open(f"images/{attachment.filename}", "wb") as f:
-                                    f.write(await resp.read())
+                    with open(f"{attachment.filename}.jpg", "wb") as f:
+                        response = requests.get(attachment.url)
+                        f.write(response.content)
+                    with open(f"{attachment.filename}.jpg", "rb") as f:
+                        files = {"image": f}
+                        response = requests.post(self.api_url, files=files)
+                    if response.status_code == 200:
+                        print("Image uploaded successfully")
+                    else:
+                        print("Failed to upload image")
+                        
 
-
-client = MyClient(intents=Intents.all())
+client = MyClient(api_url="http://0.0.0.0:8000/image/")
 client.run(BOT_TOKEN)
